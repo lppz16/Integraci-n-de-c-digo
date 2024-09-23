4c62ffebad8ddc7437683c2a8cc3715b2ec41cbb
Yan Frank <lppz1606@gmail.com>
2024-09-23 14:12:23 -0500

    Ejercicio creativo

--- Código/codigo.py
+++ Código/codigo.py
@@ -1,3 +1,17 @@
-
+print("Ejercicio propuesto por el docente:")
 num = [x for x in range (1, 11)]
-print(num)
+print(f"La lista por comprensión es: {num}")
+print(" ")
+
+print("///////////////////////////////////////////////////")
+#Ingresando número por teclado
+
+print("****Sr. Usuario, a través de este software podrás visualizar una lista por comprensión con los números que usted desee****")
+print(" ")
+z = int(input("Ingrese desde qué número quieres que vaya la lista: "))
+m = int(input("Ingrese hasta qué número quiere que vaya la lista: "))
+m+=1
+print(" ")
+
+num = [x for x in range(z, m)]
+print(f"Su lista por comprensión es la siguiente: {num}")
